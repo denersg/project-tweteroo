2ddd7000183cadfbe6d2b6540e59526caea310bd
Dener Silva <dener.ufes@gmail.com>
2022-04-24 19:32:00 -0300

    Terminada a requisição tipo post dos tweets

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import chalk from "chalk";
 
 let userData = [];
 let TWEETS = [];
+let userPicture = "";
 const app = express();//Criado o servidor
 
 app.use(cors());
@@ -16,6 +17,8 @@ app.post("/sign-up", (req, res) => {
         avatar: body.avatar
     };
 
+    userPicture = body.avatar;
+
     userData.push(userSignUp);
     res.send("OK");
     console.log(chalk.green("OK"));
@@ -26,12 +29,13 @@ app.post("/tweets", (req, res) => {
     const body = req.body;
     const newTweet = {
         username: body.username,
+        avatar: userPicture,
         tweet: body.tweet
     };
 
     TWEETS.push(newTweet);
     res.send("OK");
-    console.log(chalk.orange("OK"));
+    console.log(chalk.magenta.inverse("OK"));
     console.log("body da requisição(tweets):", body);
 });
 
